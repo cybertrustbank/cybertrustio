0adda9bcdcf37900c01591ace8d8bd013b2e77cf
jkehagias <j.kehagias1@gmail.com>
2017-12-06 18:38:00 +0800

    final

--- js/faq.js
+++ js/faq.js
@@ -1,11 +1,11 @@
-(function () {
-    $('.faq-wrap .card-header').bind('click', function () {
-        $(this).find('i').toggleClass("fa-chevron-right fa-chevron-down").parent().next().toggle(200);
+(function() {
+    $('.faq-wrap .card-header').bind('click', function() {
+        $(this).toggleClass("border-bottom").find('i').toggleClass("fa-chevron-right fa-chevron-down").parent().next().toggle(200);
         var obj = this;
-        $('.faq-wrap .card-header').each(function (item) {
+        $('.faq-wrap .card-header').each(function(item) {
             var cardbody = $(this).next();
             if (obj !== this && !cardbody.is(":hidden")) {
-                $(this).find('i').toggleClass("fa-chevron-right fa-chevron-down")
+                $(this).toggleClass("border-bottom").find('i').toggleClass("fa-chevron-right fa-chevron-down")
                 cardbody.hide();
             }
         });
